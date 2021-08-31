7ed0d5052aa82207ccdb98ec1cf59ac497b23c41
fliuntyurii <globaltura@gmail.com>
2021-08-31 20:56:36 +0000

    fixed carousel

--- js/wine-carousel.js
+++ js/wine-carousel.js
@@ -1,4 +1,4 @@
-let slideIndex = 1;
+let slideIndex = 2;
 showSlides(slideIndex);
 function plusSlides(n) {
     showSlides(slideIndex += n);
@@ -9,19 +9,16 @@ function currentSlide(n) {
 function showSlides(n) {
     let = i;
     let slides = document.getElementsByClassName("slide");
-    let dots = document.getElementsByClassName("dot");
     if (n > slides.length) {
         slideIndex = 1;
     }
     if (n < 1) {
-        slideIndex = slides.length
+        slideIndex = slides.length;
     }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = 'none';
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace("active", "");
-    }
+
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += "active";
+    
 }
